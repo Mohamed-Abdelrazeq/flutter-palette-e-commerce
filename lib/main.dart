0beb1998539c609e7d91a656ab2864853bfd1434
3abrazek <mohamedabdelrazek824@gmail.com>
2021-06-06 05:45:12 +0200

    Init Firebase Added Essential Packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,45 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Views/FutureReturn/Loading.dart';
+import 'Views/FutureReturn/SomethingWentWrong.dart';
+import 'Views/Screens/HomePage.dart';
+import 'Views/Screens/WelcomePage.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MultiProvider(
+      providers: [
+        // ChangeNotifierProvider<PageProvider>(create: (_) => PageProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return SomethingWentWrong();
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            routes: {
+              '/MyHomePage': (context) => HomePage(),
+            },
+            home: WelcomePage(),
+          );
+        }
+        return Loading();
+      },
     );
+
   }
 }
- class MyHomePage extends StatefulWidget {
-   @override
-   _MyHomePageState createState() => _MyHomePageState();
- }
-
- class _MyHomePageState extends State<MyHomePage> {
-   @override
-   Widget build(BuildContext context) {
-     return Container();
-   }
- }
