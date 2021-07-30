419faee166fcc7c4ff091a6ae9ef487c46cb7aa7
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2021-07-30 02:17:33 +0200

    Cart

--- lib/Views/Screens/CartPage.dart
+++ lib/Views/Screens/CartPage.dart
@@ -1,8 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:multivender_ecommerce_app/Views/Component/Header.dart';
+import 'package:multivender_ecommerce_app/Views/Component/MainButton.dart';
+import 'package:multivender_ecommerce_app/Views/Component/MainCard.dart';
+import 'package:multivender_ecommerce_app/Views/Component/MyTextField.dart';
 
-class CartPage extends StatelessWidget {
+import '../MyColors.dart';
+class CartPage extends StatefulWidget {
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+
+  TextEditingController _searchTextController;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double statusBar = MediaQuery.of(context).padding.top;
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Padding(
+        padding: EdgeInsets.only(top: statusBar.h,),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 28.w,right: 28.w),
+                child: Column(
+                  children: [
+                    Header(header: "Cart",),
+                    MyTextFiled(textController: _searchTextController, myIcon: Icons.search, hint: "Search"),
+                    SizedBox(height: 22.h,),
+                  ],
+                ),
+              ),
+
+              Stack(
+                alignment: Alignment.bottomCenter,
+                children:[
+                  Container(
+                  height: 410.h,
+                  width: 375.w,
+                  padding: EdgeInsets.only(bottom: 9.h,),
+                  child: GridView.count(
+                    physics: BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    padding: EdgeInsets.only(left: 28.w,right: 28.w,bottom: 100.h),
+                    children: List.generate(5, (index) {
+                      return MainCard(marginRight: 0,);
+                    }),
+                  ),
+                ),
+                  Positioned(
+                    bottom: 25.h,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 28.w),
+                      child: MainButton(text: "Checkout", btnFunction: (){
+                        //Todo
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
