798ee3307bcd7c482a62aafa72eb8f611e39723c
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2021-08-04 03:32:07 +0200

    Loading Page UI Changed

--- lib/Views/FutureReturn/Loading.dart
+++ lib/Views/FutureReturn/Loading.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:multivender_ecommerce_app/Views/MyColors.dart';
 
 
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SpinKitRotatingCircle(
-      color: Colors.redAccent,
-      size: 50.0,
+    return Container(
+      color: bgColor,
+      child: SpinKitRotatingCircle(
+        color: Colors.redAccent,
+        size: 50.0,
+      ),
     );
   }
 }
